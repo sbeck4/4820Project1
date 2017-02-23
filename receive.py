7b469578acba949a1ff5b77c7af4dd0113369c94
tarkin96 <jwk@clemson.edu>
2017-02-23 18:15:31 -0500

    Add files via upload

--- receive.py
+++ receive.py
@@ -3,20 +3,32 @@ from kivy.app           import App
 from kivy.uix.button    import Button
 from kivy.uix.label     import Label
 from kivy.uix.boxlayout import BoxLayout
-
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.image      import Image
+from kivy.uix.scatter    import Scatter
+from kivy.properties     import *
 # Import kivy osc library
 from kivy.lib.osc         import oscAPI 
 # Import clock (required by osc listener)
 from kivy.clock           import Clock
+from kivy.config import Config
+from kivy.core.window import Window
+
 
 import VideoPlayer
+
 class receiver(App):
+
     # Set ip and port to listen to
     ip = '0.0.0.0' # listens to any sender IP
     port = 5000    # listens only to this port
     play = VideoPlayer.VideoPlayer()
 
     def build(self):
+        Window.size = (1600, 900)
+        Window.borderless = True
+        Window.fullscreen = True
+        scat = Scatter()
         # Starts OSC
         oscAPI.init()  
         # Instanciates OSC listener
@@ -27,10 +39,15 @@ class receiver(App):
         oscAPI.bind(oscid, self.receivedegrees, '0')
 
         # add a label to the screen
-        #root = BoxLayout(orientation='vertical')
+        root = FloatLayout(size=(2950,1650), orientation='vertical')
         #self.label = Label(text="This is our vertical sceen!", font_size='50sp')
         #root.add_widget(self.label)
 
+        root_image = Image(source='Bottom_Screen1.jpeg', size_hint_x=None, width=1600,
+                                              size_hint_y=None, height=900,
+                                              allow_stretch = True,
+                                              keep_ratio = True)
+        root.add_widget(root_image)
         # console message - ready!
         print "Ready to receive!"
         #self.receivedegrees(0,0)
@@ -38,7 +55,7 @@ class receiver(App):
      #   self.receivedegrees(180)
      #   self.receivedegrees(270)
         # return root
-        #return root
+        return root
 #if __name__ == "__main__":
     def receivedegrees(self, vidnum, instance):
         print "received data from sender"
